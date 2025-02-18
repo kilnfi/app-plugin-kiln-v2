47eb23da700ddc7a95ac854677c167a9374da1cc
pwnh4 (@loicttn) <loic@ttn.am>
2025-02-18 16:16:34 +0100

    feat: use new boilerplate

---
+++ tests/tests/test_stake.py
@@ -0,0 +1,19 @@
+from web3 import Web3
+from tests.utils import run_test, load_contract
+
+contract_ocv1 = load_contract(
+    "0x1e68238ce926dec62b3fbc99ab06eb1d85ce0270"
+)
+
+# Test from https://etherscan.io/tx/0xea17de5f3cc81f6638c714a7be936e37d652c080c66f333df765fbe0760eaec2
+def test_stake(backend, firmware, navigator, test_name, wallet_addr):
+    data = "0xd0e30db0"
+    run_test(
+        contract_ocv1, 
+        data, 
+        backend, 
+        firmware, 
+        navigator, 
+        test_name, 
+        wallet_addr
+    )
